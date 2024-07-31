ff1dac0ea43e9c08f1684ea648d4c611c664fb19
Shahzad Qureshi <30411645+ShahzadQ@users.noreply.github.com>
2024-07-31 19:56:46 +0100

    feat: show all missing env vars in one go

--- env.ts
+++ env.ts
@@ -3,16 +3,24 @@ import { ArrayElement } from "src/types";
 
 dotenv.config();
 
-const envs = ["PORT", "DATABASE_URL"] as const;
+const requiredEnvs = ["PORT", "DATABASE_URL"] as const;
 
-export const env = envs
+const missingEnvs = [] as ArrayElement<typeof requiredEnvs>[];
+
+export const env = requiredEnvs
   .map((env) => {
-    if (typeof process.env[env] === "undefined")
-      throw new Error(`You need to set ${env} in .env`);
-
+    if (typeof process.env[env] === "undefined") missingEnvs.push(env);
     return { [env]: process.env[env] };
   })
   .reduce((a, v) => ({ ...a, ...v })) as Record<
-  ArrayElement<typeof envs>,
+  ArrayElement<typeof requiredEnvs>,
   string
 >;
+
+if (missingEnvs.length > 0) {
+  throw new Error(
+    `You are missing the following env variables: ${missingEnvs.join(
+      ", "
+    )}. Add them to .env.`
+  );
+}
