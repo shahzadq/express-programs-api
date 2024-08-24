a4ef9270da0211e112b31de1554fcd24ae5eb6d0
Shahzad Qureshi <30411645+ShahzadQ@users.noreply.github.com>
2024-08-24 14:54:39 +0100

    chore: fix db seed script

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -1,9 +1,11 @@
-import { programs } from "~/models/programs";
+import { programs } from "~/routers/programs/programs.schemas";
+import { Program } from "~/routers/programs/programs.types";
 import { db } from "~/services/db";
-import { Program } from "~/types/programs";
 import rawExamplePrograms from "../example-programs.json";
 
 async function main() {
+  console.log("Reading and parsing 'example-programs.json'...");
+
   const examplePrograms = (rawExamplePrograms as Program[]).map(
     ({ id, ...program }) => program
   );
